202ca75bcb9524b0bd01467036746a5949b83a55
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-10-22 11:51:06 +0700

    Menambahkan express dan menambhakan get "hello world"

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3001, () => {
+    console.log('Example app listening on port 3001!');
+});
+
